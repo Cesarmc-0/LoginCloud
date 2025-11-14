37d4a6839eda6971814c66ba0b7d861f7c1cba8b
CESAR <CESAR@DESKTOP-7GQ49RU>
2025-11-14 09:57:51 -0500

    Nuevos Estilos

--- src/model/User.java
+++ src/model/User.java
@@ -1,25 +1,25 @@
-package model;
-    public class User {
-    private int id;    
-    private String username;
-    private String password;
-    private String role;
+// package model;
+//     public class User {
+//     private int id;    
+//     private String username;
+//     private String password;
+//     private String role;
   
 
-    public User(int id, String username, String password, String role) {
-        this.id = id;
-        this.username = username;
-        this.password = password;
-        this.role = role;
-    }
-    public int getId() {
-        return id;
-    }
+//     public User(int id, String username, String password, String role) {
+//         this.id = id;
+//         this.username = username;
+//         this.password = password;
+//         this.role = role;
+//     }
+//     public int getId() {
+//         return id;
+//     }
 
-    public String getusername() {
-        return username;
-    }
-}
+//     public String getusername() {
+//         return username;
+//     }
+// }
 
 
 
